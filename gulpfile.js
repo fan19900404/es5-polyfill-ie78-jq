2cac48a44dba24c3c1caab30943ee5ba7d0674ca
fan19900404 <fan19900404@163.com>
2018-04-21 17:45:58 +0800

    变量名压缩

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task("script", function() {
   gulp
     .src("src/index.js")
     // 2. 压缩文件
-    .pipe(uglify({ mangle: false }))
+    .pipe(uglify())
     // 3. 另存压缩后的文件
     .pipe(rename("es5-polyfill-ie78-jq.min.js"))
     .pipe(gulp.dest("dist"));
